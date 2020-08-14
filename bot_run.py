0dbbf87f059f9fc909d1c0726ccc55a36a836aa5
netyo715 <netyo715@gmail.com>
2020-08-14 15:03:39 +0900

    bot_run.pyの修正

--- bot_run.py
+++ bot_run.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     token = config["BOT"]["Token"]
     prefix = config["BOT"]["CommandPrefix"]
 
-    bot = bot.TeamMakeBot()
+    bot = bot.TeamMakeBot(prefix)
     bot.run(token)
