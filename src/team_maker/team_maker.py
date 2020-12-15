01899f3c6ed4c85bd931c02fd2a98ec41eaff76e
Sohei Nishimura <netyo715@gmail.com>
2020-12-16 01:04:15 +0900

    バグ修正

--- src/team_maker/team_maker.py
+++ src/team_maker/team_maker.py
@@ -73,7 +73,7 @@ class TeamMaker():
         shuffled_members.sort(key=lambda member: member.priority, reverse=True)
 
         # この後チームに振り分けるメンバー
-        team_member = random.sample(shuffled_members[:limit], limit)
+        team_member = random.sample(shuffled_members[:limit], min(limit, len(shuffled_members)))
         # 余り
         self.remainder = sorted(shuffled_members[limit:], key=lambda member: member.name)
 
